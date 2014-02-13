d40cb03503f6b040a4c3130ad241cc50e33f0f3f
Maksym Tashchuk <maxim.taschuk@gmail.com>
2014-02-14 00:47:15 +0200

    changes

--- fain_lessons/src/unit1/HelloJava.java
+++ fain_lessons/src/unit1/HelloJava.java
@@ -9,6 +9,6 @@ package unit1;
  */
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello Java!!!");
     }
 }

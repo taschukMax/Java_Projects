255675c584dd0d2a77d6082c18b8ebdc08ff1ab9
Maksym Tashchuk <maxim.taschuk@gmail.com>
2014-02-14 00:34:00 +0200

    fain lessons

---
+++ fain_lessons/src/unit1/HelloJava.java
@@ -0,0 +1,14 @@
+package unit1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maksym
+ * Date: 2/5/14
+ * Time: 1:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}

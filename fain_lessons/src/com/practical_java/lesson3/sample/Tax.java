e221e7bb9aaa1a3e6fcb489d81125ec4bde8347f
Maksym Tashchuk <maxim.taschuk@gmail.com>
2014-02-16 17:14:07 +0200

    More refactoring:

---
+++ fain_lessons/src/com/practical_java/lesson3/sample/Tax.java
@@ -0,0 +1,20 @@
+package com.practical_java.lesson3.sample;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maksym
+ * Date: 2/16/14
+ * Time: 5:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tax {
+    double grossIncome;
+    String state;
+    int dependents;
+
+    public double calcTax() {
+
+        return 234.55;
+    }
+
+}
---
+++ fain_lessons/src/com/practical_java/lesson3/tryit/NJTax.java
@@ -0,0 +1,22 @@
+package com.practical_java.lesson3.tryit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maksym
+ * Date: 2/16/14
+ * Time: 5:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NJTax extends Tax {
+    public double calcTax() {
+        double  stateTax=0;
+        if (grossIncome < 30000) {
+            stateTax=grossIncome*0.05;
+        }
+        else{
+            stateTax= grossIncome*0.06;
+        }
+
+        return stateTax - 500;
+    }
+}

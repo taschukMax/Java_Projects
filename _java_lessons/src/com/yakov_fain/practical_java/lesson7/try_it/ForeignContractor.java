ebf3aca33653a9948c8ec7f89906c56af84e9dd7
taschukMax <maxim.taschuk@gmail>
2014-02-26 14:45:53 +0200

    Started lesson 7, try it section

---
+++ _java_lessons/src/com/yakov_fain/practical_java/lesson7/try_it/ForeignContractor.java
@@ -0,0 +1,15 @@
+package com.yakov_fain.practical_java.lesson7.try_it;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: maksym.tashchuk
+ * Date: 2/26/14
+ * Time: 2:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ForeignContractor extends Person {
+
+    public ForeignContractor(String name) {
+        super(name);
+    }
+}

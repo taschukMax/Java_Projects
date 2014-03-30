50755c1aad3a2dd68fac33219f878eb53532ad26
Maksym Tashchuk <maxim.taschuk@gmail.com>
2014-03-31 01:19:57 +0300

    Lesson 14: Collection API

---
+++ _java_lessons/src/com/yakov_fain/practical_java/lesson14/Main.java
@@ -0,0 +1,11 @@
+package com.yakov_fain.practical_java.lesson14;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maksym
+ * Date: 3/31/14
+ * Time: 12:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}

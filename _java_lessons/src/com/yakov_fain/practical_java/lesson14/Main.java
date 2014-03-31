1c228c395f981c5d3b6bb4058e547caca86e6021
taschukMax <maxim.taschuk@gmail>
2014-03-31 11:48:40 +0300

    Lesson 14, Try It

--- _java_lessons/src/com/yakov_fain/practical_java/lesson14/Main.java
+++ _java_lessons/src/com/yakov_fain/practical_java/lesson14/Main.java
@@ -1,5 +1,8 @@
 package com.yakov_fain.practical_java.lesson14;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Maksym
@@ -8,4 +11,18 @@ package com.yakov_fain.practical_java.lesson14;
  * To change this template use File | Settings | File Templates.
  */
 public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> passengersList = new LinkedList<String>();
+        passengersList.add("Maxim");
+        passengersList.add("Nadya");
+        passengersList.add("Polina");
+        ListIterator<String> iterator = passengersList.listIterator();
+
+        System.out.println(iterator.next());
+        iterator.add("VIP");
+        iterator.previous();
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+    }
 }
